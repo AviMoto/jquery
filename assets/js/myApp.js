daca333407c048e24b70fec7f03bbc030f96fb11
Avi <Avi@admintheweb.com>
2014-03-24 00:59:57 +0200

    sortable

--- assets/js/myApp.js
+++ assets/js/myApp.js
@@ -2,6 +2,8 @@
  * start to run this function when the document is ready.
  */
 $(function(){
-	$(".product-grid li").draggable({ containment: "#catalog-page" });
-	
+	$(".product-grid").sortable({
+      placeholder: ".product-grid li"
+    });
+	$( ".product-grid" ).disableSelection();
 });
