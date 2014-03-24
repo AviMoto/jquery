325db69c6c50e359da0585d466a06b1a7651e14e
Avi <Avi@admintheweb.com>
2014-03-24 23:39:29 +0200

    add accordion

--- assets/js/myApp.js
+++ assets/js/myApp.js
@@ -2,6 +2,7 @@
  * start to run this function when the document is ready.
  */
 $(function(){
+	$("#catalog-page").accordion();
 	$(".product-grid").sortable({
       placeholder: ".product-grid li"
     });
