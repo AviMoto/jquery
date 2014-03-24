008c8337ebae8bfb6aa9e92303bce54bd1e4f791
Avi <Avi@admintheweb.com>
2014-03-24 23:42:20 +0200

    sortable accordion

--- assets/js/myApp.js
+++ assets/js/myApp.js
@@ -1,10 +1,21 @@
 /**
  * start to run this function when the document is ready.
  */
-$(function(){
-	$("#catalog-page").accordion();
+$(function() {
+	$("#catalog-page").accordion({
+		header : "> div > h2",
+		heightStyle : "content"
+	}).sortable({
+		axis : "y",
+		handle : "h2",
+		stop : function(event, ui) {
+			// IE doesn't register the blur when sorting
+			// so trigger focusout handlers to remove .ui-state-focus
+			ui.item.children("h3").triggerHandler("focusout");
+		}
+	});
 	$(".product-grid").sortable({
-      placeholder: ".product-grid li"
-    });
-	$( ".product-grid" ).disableSelection();
+		placeholder : ".product-grid li"
+	});
+	$(".product-grid").disableSelection();
 });
