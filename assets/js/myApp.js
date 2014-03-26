9f1d297a259c04bdb5094a95c05c7a263faa9f6e
Avi <Avi@admintheweb.com>
2014-03-26 23:35:01 +0200

    add checkout cart

--- assets/js/myApp.js
+++ assets/js/myApp.js
@@ -1,7 +1,32 @@
+function getPrice(line){
+	var price;
+	var pattern = /[0-9,]+.₪/;
+	price = pattern.exec($(line).text());
+	price = price[0].replace(",","");
+	return parseInt(price.slice(0,price.length-2));
+}
 /**
  * start to run this function when the document is ready.
  */
 $(function() {
+	var Checkout = (function(){
+		return {
+			checkout: function(totalCost){
+				try{
+					if(window.localStorage){
+						localStorage["totalCost"] = totalCost;
+						window.location.href = "checkout.html";
+					}
+				} catch (e) {
+					alert("Problem: can't complete without local storage");
+				}
+			}
+		};
+	})();
+	var cartSum =0;
+	function updateCartTotal(){
+		$("#cartSum").text(cartSum);
+	}
 	$("#catalog-page").accordion({
 		header : "> div > h2",
 		heightStyle : "content"
@@ -14,8 +39,27 @@ $(function() {
 			ui.item.children("h3").triggerHandler("focusout");
 		}
 	});
-	$(".product-grid").sortable({
-		placeholder : ".product-grid li"
+	$("li.item").draggable({
+		appendTo : "body",
+		helper : "clone"
 	});
-	$(".product-grid").disableSelection();
+	$("#shoppingCart ol").droppable({
+		activeClass : "ui-state-default",
+		hoverClass : "ui-state-hover",
+		accept : ":not(.ui-sortable-helper)",
+		drop : function(event, ui) {
+			$(this).find(".placeholder").remove();
+			$("<li></li>").append(ui.draggable.text() + '<span class="ui-button-icon-primary ui-icon ui-icon-closethick"></span>').appendTo(this);
+			$("li span.ui-icon").last().click(function(){
+				cartSum -= getPrice($(this).parent());
+				$(this).parent().remove();
+				updateCartTotal();
+			});
+			cartSum += getPrice(ui.draggable);
+			updateCartTotal();
+		}
+	});
+	$("#checkout").click(function(){
+		Checkout.checkout(cartSum);
+	});
 });
